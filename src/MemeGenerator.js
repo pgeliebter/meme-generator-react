beab34a06ad3312dedd8c193d6faa826809f985d
Pinny Geliebter <pgibralter@gmail.com>
2021-11-15 08:30:26 -0500

    adds top and bottom text and rndmimage to state

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
+/**
+ * Initialize state to save the following data:
+ *      top text
+ *      bottom text
+ *      random image (intialize with "http://i.imgflip.com/1bij.jpg")
+ */
+
 class MemeGenerator extends Component {
 	constructor() {
 		super()
-		this.state = {}
+		this.state = {
+			topText: '',
+			bottomText: '',
+			randomImg: 'http://i.imgflip.com/1bij.jpg'
+		}
 	}
 	render() {
 		return <div>I am a placeholder for meme</div>
