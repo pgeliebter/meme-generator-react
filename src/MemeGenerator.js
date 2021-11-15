c56d47faab37ef599fd797031b38371b39b4369d
Pinny Geliebter <pgibralter@gmail.com>
2021-11-15 08:48:07 -0500

    adds fetch to meme api

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
 /**
- * Initialize state to save the following data:
- *      top text
- *      bottom text
- *      random image (intialize with "http://i.imgflip.com/1bij.jpg")
+ * We'll be using an API that provides a bunch of meme images.
+ * 
+ * Your task:
+ * make an API call to "https://api.imgflip.com/get_memes" and save the 
+ * data that comes back (`response.data.memes`) to a new state property
+ * called `allMemeImgs`. (The data that comes back is an array)
  */
 
 class MemeGenerator extends Component {
@@ -15,9 +17,21 @@ class MemeGenerator extends Component {
 			bottomText: '',
 			randomImg: 'http://i.imgflip.com/1bij.jpg'
 		}
+		this.getMemeImages = this.getMemeImages.bind(this)
 	}
+	getMemeImages() {
+		fetch('https://api.imgflip.com/get_memes')
+			.then((response) => response.json())
+			.then((response) => console.log(response.data))
+	}
+
 	render() {
-		return <div>I am a placeholder for meme</div>
+		return (
+			<div>
+				I am a placeholder for meme
+				<button onClick={this.getMemeImages}>Get memes</button>
+			</div>
+		)
 	}
 }
 
